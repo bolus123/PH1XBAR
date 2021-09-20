19932b726d476b7f84a2e92193d71bc17e6f2f5d
Yuhui Yao <bolus123@users.noreply.github.com>
2021-09-20 11:34:35 -0500

    update
    add alias to getCC.XBAR

--- R/getCC.XBAR.R
+++ R/getCC.XBAR.R
@@ -1,14 +1,14 @@
 getCC.XBAR <- function(
-      FAP0
-			,m
-			,var.est = c('S', 'MR')
-			,ubCons = 1 
-			,method = c('exact', 'BA')
-			,interval = c(1, 5)
-			,nsim = 10000
-			,nu = m - 1 
-			,lambda = 1
-      ,verbose = FALSE
+		FAP0
+		,m
+		,var.est = c('S', 'MR')
+		,ubCons = 1 
+		,method = c('exact', 'BA')
+		,interval = c(1, 5)
+		,nsim = 10000
+		,nu = m - 1 
+		,lambda = 1
+		,verbose = FALSE
   ){
    
   var.est <- var.est[1]
@@ -28,5 +28,35 @@ getCC.XBAR <- function(
     stop('Unexpected variance estimator. The method must be in c("exact", "BA"). The program will stop.')
   
   }
+
+}
+
+getCC <- function(
+		FAP0
+		,m
+		,var.est = c('S', 'MR')
+		,ubCons = 1 
+		,method = c('exact', 'BA')
+		,interval = c(1, 5)
+		,nsim = 10000
+		,nu = m - 1 
+		,lambda = 1
+		,verbose = FALSE
+  ) {
+ 
+	warnings("This function has been renamed.  Please use getCC.XBAR")
+ 
+	getCC.XBAR(
+		FAP0 = FAP0
+		,m = m
+		,var.est = var.est
+		,ubCons = ubCons 
+		,method = method
+		,interval = interval
+		,nsim = nsim
+		,nu = nu
+		,lambda = lambda
+		,verbose = verbose
+	)
 
 }
